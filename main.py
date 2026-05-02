53a0443cb2e51ba108e5df2285a16c29ce3376fd
Elena Kolesnikova <koleslena@yandex.ru>
2026-05-02 20:43:54 +0500

    train datd

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import argparse
 import torch
 from torch.nn import functional as F
 
-from common.datasets_util import get_pos_datasets, get_split_pos_datasets, Datasources
+from common.datasets_util import Datasources
 from common.loss import FocalLoss
 from common.model_trainer import Trainer
+from common.pos_datasets import INDEX_PAD, PosDataloaders
 from pos_taggers import cnn_pos_tagger
 from pos_taggers import bilstm_pos_tagger
 from sanskrit_tagger.pos_tagger import POSTagger
@@ -13,7 +14,7 @@ from sanskrit_tagger.tagger_factory import get_pos_tagger
 
 from auto_push import git_push_results
 
-_train = True
+_train = False
 _push = True
 
 test_pos_sentences = [
@@ -31,12 +32,12 @@ model_name="pos_tagger"
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", type=str, default='../../texts/')
-    parser.add_argument("--vocab_size", type=int, default=42) 
-    parser.add_argument("--labels_num", type=int, default=734) 
-    parser.add_argument("--max_batches_per_epoch_train", type=int, default=400)
-    parser.add_argument("--max_batches_per_epoch_val", type=int, default=100)
+    parser.add_argument("--vocab_size", type=int, default=58) 
+    parser.add_argument("--labels_num", type=int, default=745) 
+    parser.add_argument("--max_batches_per_epoch_train", type=int, default=100000)
+    parser.add_argument("--max_batches_per_epoch_val", type=int, default=10000)
     parser.add_argument("--epoch_n", type=int, default=50)
-    parser.add_argument("--embedding_size", type=int, default=64)
+    parser.add_argument("--embedding_size", type=int, default=128)
     parser.add_argument("--device", type=str, default='cpu')
     parser.add_argument("--model", type=str, default='bilstm') # bilstm, cnn
     parser.add_argument("--full_pos", type=bool, default=True)
@@ -46,19 +47,18 @@ def main():
     model_name = cnn_pos_tagger.get_model_name(full_pos=args.full_pos) if args.model == 'cnn' else bilstm_pos_tagger.get_model_name(full_pos=args.full_pos)
 
     if _train:
-        datasets = get_split_pos_datasets([
-                                            Datasources.MAHABHARATA, 
-                                           Datasources.RAMAYANA, 
-                                           Datasources.AMARAKOSHA, 
-                                           Datasources.HITOPADESHA, 
-                                           Datasources.SHIVAPURANA, 
-                                           Datasources.BHAGAVATAPURANA, 
-                                           Datasources.VISHNUPURANA
-                                           ], 
-                            full_pos=args.full_pos,
-                            data_path=args.data_path)
-        
-        datasets.save_data()
+
+        texts = [
+                Datasources.MAHABHARATA, 
+               Datasources.RAMAYANA, 
+               Datasources.AMARAKOSHA, 
+               Datasources.HITOPADESHA, 
+               Datasources.SHIVAPURANA, 
+               Datasources.BHAGAVATAPURANA, 
+                Datasources.VISHNUPURANA
+                ]
+
+        datasets = PosDataloaders(texts, max_tokens=1500)
         
         if args.model == 'cnn':
             model = cnn_pos_tagger.get_model(datasets.vocab_size, datasets.labels_num, embedding_size=args.embedding_size)
@@ -66,7 +66,7 @@ def main():
             model = bilstm_pos_tagger.get_model(datasets.vocab_size, datasets.labels_num, embedding_size=args.embedding_size)
 
         trainer = Trainer(datasets, model,
-                        FocalLoss(gamma=2), 
+                        FocalLoss(gamma=2.5, ignore_index=INDEX_PAD), 
                         output_model_name=model_name, 
                         device=args.device,
                         with_metrics=args.with_metrics,
@@ -81,14 +81,14 @@ def main():
 
         model.load_state_dict(torch.load(f'output/{model_name}.pth'))
 
-        pos_tagger = POSTagger(model, datasets.char2id, datasets.unique_tags, datasets.max_sent_len, datasets.max_origin_token_len)
+        pos_tagger = POSTagger(model, datasets.char2id, datasets.unique_tags)
     else:
         model = cnn_pos_tagger.get_model(args.vocab_size, args.labels_num, embedding_size=args.embedding_size) if args.model == 'cnn' else \
             bilstm_pos_tagger.get_model(args.vocab_size, args.labels_num, embedding_size=args.embedding_size)
 
         model.load_state_dict(torch.load(f'output/{model_name}.pth'))
 
-        pos_tagger = get_pos_tagger(model, args.vocab_size, args.labels_num)
+        pos_tagger = get_pos_tagger(model)
 
     for sent_tokens, sent_tags in zip(test_pos_sentences_tokenized, pos_tagger(test_pos_sentences)):
         print(' '.join('{}-{}'.format(tok, tag) for tok, tag in zip(sent_tokens, sent_tags)))
