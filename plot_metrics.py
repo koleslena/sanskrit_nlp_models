1bf70982bedf614ba0fc4b590835efea52ef8d5b
Elena Kolesnikova <koleslena@yandex.ru>
2026-05-06 01:31:45 +0500

    add metrics plot

--- plot_metrics.py
+++ plot_metrics.py
@@ -2,12 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-model_release='segmenter_model_metrics_1777946740.0313993'
+model_release='pos_tagger_model_metrics_1778006522.3743942'
+
+is_pos=model_release.startswith('pos')
+file_name='training_log' if is_pos else 'segmenter_train_log'
 # Путь к твоему файлу
-file_path = f'metrics/{model_release}/segmenter_train_log.csv'
+file_path = f'metrics/{model_release}/{file_name}.csv'
 save_path = f'metrics/{model_release}/learning_curves.png'
 
-def plot_training_history(csv_path):
+def plot_training_history(csv_path, is_pos):
     # 1. Загрузка данных
     df = pd.read_csv(csv_path)
     
@@ -23,14 +26,23 @@ def plot_training_history(csv_path):
     ax1.legend()
     ax1.grid(True, alpha=0.3)
 
-    # 3. График Accuracy (Точность)
-    ax2.plot(df['epoch'], df['accuracy_em'], label='Exact Match (EM)', color='#00b894', linewidth=2)
-    ax2.plot(df['epoch'], df['accuracy_char'], label='Char Accuracy', color='#55efc4', linewidth=2)
-    ax2.set_title('Accuracy Metrics', fontsize=14, fontweight='bold')
-    ax2.set_xlabel('Epoch', fontsize=12)
-    ax2.set_ylabel('Accuracy', fontsize=12)
-    ax2.legend(loc='lower right')
-    ax2.grid(True, alpha=0.3)
+    if is_pos:
+        # 3. График F1 macro
+        ax2.plot(df['epoch'], df['val_f1'], label='F1 score (macro)', color='#00b894', linewidth=2)
+        ax2.set_title('F1 Metrics', fontsize=14, fontweight='bold')
+        ax2.set_xlabel('Epoch', fontsize=12)
+        ax2.set_ylabel('F1 score', fontsize=12)
+        ax2.legend(loc='lower right')
+        ax2.grid(True, alpha=0.3)
+    else:
+        # 3. График Accuracy (Точность)
+        ax2.plot(df['epoch'], df['accuracy_em'], label='Exact Match (EM)', color='#00b894', linewidth=2)
+        ax2.plot(df['epoch'], df['accuracy_char'], label='Char Accuracy', color='#55efc4', linewidth=2)
+        ax2.set_title('Accuracy Metrics', fontsize=14, fontweight='bold')
+        ax2.set_xlabel('Epoch', fontsize=12)
+        ax2.set_ylabel('Accuracy', fontsize=12)
+        ax2.legend(loc='lower right')
+        ax2.grid(True, alpha=0.3)
 
     # Улучшаем оформление
     plt.tight_layout()
@@ -42,6 +54,6 @@ def plot_training_history(csv_path):
 
 if __name__ == "__main__":
     if os.path.exists(file_path):
-        plot_training_history(file_path)
+        plot_training_history(file_path, is_pos)
     else:
         print(f"Файл не найден по пути: {file_path}")
