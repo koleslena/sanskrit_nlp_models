1301035d2fb643fbdd4f374bbfb4fdbed40d6e25
Elena Kolesnikova <koleslena@yandex.ru>
2026-06-08 23:26:14 +0300

    max_tokens_per_batch=6000

--- main_seg.py
+++ main_seg.py
@@ -31,7 +31,7 @@ def main():
     parser.add_argument("--epoch_n", type=int, default=100)
     parser.add_argument("--train_tuning", type=bool, default=False)
     parser.add_argument("--embedding_size", type=int, default=256)
-    parser.add_argument("--max_tokens_per_batch", type=int, default=4000)
+    parser.add_argument("--max_tokens_per_batch", type=int, default=6000)
     parser.add_argument("--device", type=str, default=None)
     parser.add_argument("--model_name", type=str, default=None)
     parser.add_argument("--version", type=str, default=None)
