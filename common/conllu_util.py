d48487b8e5ab50600add0f3144262ab5f177e642
Elena Kolesnikova <koleslena@yandex.ru>
2026-06-14 02:21:58 +0300

    fix tasil and abl

--- common/conllu_util.py
+++ common/conllu_util.py
@@ -27,10 +27,19 @@ def read_pos_conllu_file(nfiles):
     # Convert to a list of dictionaries for DataFrame creation
     all_tokens = []
     for sentence in sentences:
+        with_tasil = False
+        text = transliterate(sentence.metadata['text'], sanscript.IAST, sanscript.SLP1)
+        if 'taH ' in text or 'taSca' in text or 'tas' in text or 'to ' in text:
+            with_tasil = True
         for token in sentence:
             token['sent_id'] = sentence.metadata['sent_id']
             form = token['form'] if len(token['form']) > 0 and token['form'] != '_' else token['lemma']
             token['form_slp1'] = transliterate(form, sanscript.IAST, sanscript.SLP1)
+            if with_tasil and token['form_slp1'].endswith('At'):
+                abls = set([token['form_slp1'][:-2] + 'atas', token['form_slp1'][:-2] + 'ato', token['form_slp1'][:-2] + 'ataS', token['form_slp1'][:-2] + 'ataH'])
+                found = [a for a in abls if a in text]
+                if len(found) > 0:
+                    token['form_slp1'] = token['form_slp1'][:-2] + 'ataH'
             all_tokens.append(token)
 
     # Create a Pandas DataFrame
@@ -74,11 +83,20 @@ def read_split_conllu_file(nfiles, transliterate=False):
     ids = [sent.metadata['sent_id'] for sent in sentences]
     splited = []
     for sentence in sentences:
+        with_tasil = False
+        text = IASTToSlp(sentence.metadata['text'])
+        if 'taH ' in text or 'taSca' in text or 'tas' in text or 'to ' in text:
+            with_tasil = True
         all_tokens = []
         for token in sentence:
             if str(token['id']).isdigit():
                 form  = token['form'] if len(token['form']) > 0 and token['form'] != '_' else token['lemma']
                 token_form = IASTToSlp(form) if transliterate else form
+                if with_tasil and token_form.endswith('At'):
+                    abls = set([token_form[:-2] + 'atas', token_form[:-2] + 'ato', token_form[:-2] + 'ataS', token_form[:-2] + 'ataH'])
+                    found = [a for a in abls if a in text]
+                    if len(found) > 0:
+                        token_form = token_form[:-2] + 'ataH'
                 token_form = token_form.replace("'", 'a', 1)
                 all_tokens.append(token_form)
                 all_tokens.append("-" if token['feats'] == {'Case': 'Cpd'} else " ")
